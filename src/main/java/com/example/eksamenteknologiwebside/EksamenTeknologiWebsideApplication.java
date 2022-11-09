eb5b7aceceefa77d2e085e586a795cccb27e7b27
AsHansen88 <Ande970s@stud.kea.dk>
2022-11-09 13:00:56 +0100

    Start template fra Janus

---
+++ src/main/java/com/example/eksamenteknologiwebside/EksamenTeknologiWebsideApplication.java
@@ -0,0 +1,13 @@
+package com.example.eksamenteknologiwebside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EksamenTeknologiWebsideApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EksamenTeknologiWebsideApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/eksamenteknologiwebside/EksamenTeknologiWebsideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eksamenteknologiwebside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EksamenTeknologiWebsideApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
